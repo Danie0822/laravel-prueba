192f0cb418f9f4cbb53f57eca3b9e6d795c22679
Danie0822 <20190652@ricaldone.edu.sv>
2024-11-14 14:29:01 -0600

    cambios de base

---
+++ DBPrueba.sql
@@ -0,0 +1,8 @@
+-- Eliminar la base de datos si ya existe
+DROP DATABASE IF EXISTS trabajoprueba2;
+
+-- Crear la base de datos
+CREATE DATABASE trabajoprueba2;
+
+-- Usar la base de datos
+USE trabajoprueba2;
